cb3ad1bedd2d93ba0b6930d98c792b47435fdd43
Roberto Gonzalez <rob.gonzalez3@gmail.com>
2020-09-15 12:06:57 +0200

    feat(first step): added a form using a class component that submits the answer in the console
    added a class component that renders a form from the input.json file. the input fields appear in the
    browser when the upper one is already filled. it submits the response by a console.log(). Added also
    some bootstrap styles, margins and changed the font to montserrat from google-fonts.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,21 +5,20 @@ import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import input from "../input.json";
 
-import InputForm from "./inputs";
 
-import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBInput } from "mdbreact";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import './App.css'
 class App extends Component {
   constructor() {
     super();
     this.state = {
       inputShow: input.inputFields.slice(0, 1),
       input,
-      content: '',
+      content: { name: "", surname: "", age: "" },
     };
     this.numberOfInputs = 1;
   }
@@ -31,21 +30,16 @@ class App extends Component {
     });
   };
 
-  handleInputChange = (e) => {
-    console.log('traza target', e.target);
-    this.setState({ content: e.target.content });
-    console.log('traza state',this.state.content);
-    // const { name, value } = e.target;
-    // this.setState({ [name]: value });
+  handleInputChange = (e, id) => {
+    this.setState({ content: { ...this.state.content, [id]: e.target.value } })
   };
+
 
   handleFormSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state);
-    console.log("traza", { content: e.target.content });
+    console.log(this.state.content);
   };
   render() {
-    console.log("traza", this.state.content);
     return (
       <>
         <Container as="main">
@@ -54,29 +48,24 @@ class App extends Component {
               <Form onSubmit={this.handleFormSubmit}>
                 {this.state.inputShow.map((input) => (
                   <Form.Group key={input.id}>
+                    
                     <Form.Label>{input.label}</Form.Label>
                     <Form.Control
-                      // onChange={this.handleInputChange}
                       onChange={(e) => {
-                        this.handleInputChange(e);
+                        this.handleInputChange(e, input.id);
                         this.addNewInput();
                       }}
                       id={input.id}
                       placeholder={input.placeholder}
                       type={input.type}
                       maxLength={input.maxLength}
-                      value={this.state.content}
+                      value={this.state.content[input.id]}
                     />
                   </Form.Group>
-                  // <InputForm
-                  //   key={input.id}
-                  //   {...this.state.value}
-                  //   {...input}
-                  //   handleInputChange={this.handleInputChange}
-                  //   addNewInput={this.addNewInput}
-                  // />
                 ))}
-                <Button type="submit">Submit</Button>{" "}
+                <Button variant="success" type="submit">
+                  Submit
+                </Button>
               </Form>
             </Col>
           </Row>
